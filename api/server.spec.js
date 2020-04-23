761e2eaf9dfd11c9b9c25e3ef3d721a6da21499c
Dylan Collins <dcollins5590@students.mc3.edu>
2020-04-23 17:19:09 -0400

    finished testing all end point with a minimum of 2 test

--- api/server.spec.js
+++ api/server.spec.js
@@ -1,3 +1,18 @@
 const request = require('supertest');
 const server = require('../api/server.js');
 const db = require('../data/db-config.js');
+
+describe('testing server.js', ()=>{
+    describe('testing the GET /', ()=>{
+        it('should return a 200 status', ()=>{
+            return request(server)
+            .get('/')
+            .then(res => expect(res.status).toBe(200))
+        })
+        it('should retru api running from server', ()=>{
+            return request(server)
+            .get('/')
+            .then(res => expect(res.body.message).toBe('api running from server'))
+        })
+    })
+})
